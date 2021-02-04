dc2118384d5582deeccd4fbeb1f17d7d7f7d8400
Hugo Vargas <vargas88hugo@gmail.com>
2021-02-04 14:41:24 -0500

    feat: create insert linked list exercise

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - Inserts a number into a sorted singly-linked list.
+ * @head: A pointer the head of the linked list.
+ * @number: The number to insert
+ * Return: If fails - NULL Otherwise - a pointer to the new node.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *newnode, *head2;
+
+	head2 = *head;
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = number;
+	if (*head == NULL)
+	{
+		newnode->next = NULL;
+		*head = newnode;
+		return (newnode);
+	}
+	if (head2->n > newnode->n)
+	{
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
+	}
+	while (head2->next != NULL)
+	{
+		if (head2->next->n >= newnode->n)
+		{
+			newnode->next = head2->next;
+			head2->next = newnode;
+			return (newnode);
+		}
+		head2 = head2->next;
+	}
+	newnode->next = head2->next;
+	head2->next = newnode;
+	return (newnode);
+}
---
+++ 0x01-insert_in_sorted_linked_list/README.md
@@ -0,0 +1,2 @@
+# 0x01. Insert in sorted linked list
+this is a project for inserting a node in a linked list
